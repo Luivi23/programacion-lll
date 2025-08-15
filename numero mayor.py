9419f4e7fe68107af929a04d06582322ba88afa2
Luivi23 <luis.iver643@gmail.com>
2025-08-15 09:49:59 -0600

    primer codigo de python

---
+++ numero mayor.py
@@ -0,0 +1,10 @@
+numero1 =int(input("ingrese el primer numero: "))
+numero2 =int(input("ingrese el segundo numero: "))
+
+
+
+
+if numero1 > numero2:
+    print(numero1," es mayor que ",numero2)
+else:
+    print(numero2," es mayor que ",numero1)
