b24af0af85de1155c2b8a92c29b9dac1b10368c1
Luivi23 <luis.iver643@gmail.com>
2025-08-22 09:16:50 -0600

    lista sumada  22/08/25

---
+++ lista01.py
@@ -0,0 +1,14 @@
+print("----------------------------------")
+print("lista - sumar todos ")
+print("----------------------------------")
+#solicita un numrro final de la lists
+num1 =int (input("ingrse el numero hasta 100: "))
+
+#crea la lista desde uno hara el $num1
+lista = list(range(1, num1+1))
+
+#calcular suma
+resultado=sum(lista)
+
+#mostrsr el resultado
+print(f"la suma de la lista hasta {num1} es {resultado}")
