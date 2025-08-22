1107e4b026ed4d13849c4324fb1fb681426fc05a
Luivi23 <luis.iver643@gmail.com>
2025-08-22 09:47:35 -0600

    funcion de cuadrado

---
+++ funciones01.py
@@ -0,0 +1,9 @@
+#crea una funcion
+def cuadrado_numero(num1):
+    return num1 ** 2
+
+#solicitar la variable
+num1 =int(input("introduzca el nuemero: "))
+
+#imprimir resultado
+print(f'el cuadrado de {num1} es {cuadrado_numero(num1)}')
